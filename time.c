cbb5e9608f59a1351491a5a58089df959acf953f
Vincent3164TLS <vincent.soucourre@orange.fr>
2023-12-18 11:15:22 +0100

    modified

--- time.c
+++ time.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <time.h>
 
-void printElapsedTime() {
-    clock_t start_time = clock();
+void printTime() {
+    clock_t start= clock();
 
     while (1) {
-        clock_t current_time = clock();
-        double elapsed_time = (double)(current_time - start_time) / CLOCKS_PER_SEC;
+        clock_t current = clock();
+        double time = (double)(current - start) / CLOCKS_PER_SEC;
 
-        printf("Elapsed Time: %.2f seconds\n", elapsed_time);
+        printf("temps depuis activation: %.2f seconds\n", time);
 
-        while ((double)(clock() - current_time) / CLOCKS_PER_SEC < 1.0) {}
+        while ((double)(clock() - current) / CLOCKS_PER_SEC < 1.0) {}
     }
 }
 
 int main() {
-    printElapsedTime();
+    printTime();
 
     return 0;
 }
